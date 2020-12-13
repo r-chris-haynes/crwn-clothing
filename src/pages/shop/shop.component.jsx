c306b31e315e5f3b9a02ef47e5afaba52ef770c0
chris haynes <r.chris.haynes@gmail.com>
2020-12-13 10:00:52 -0500

    shop page rendering out all of our information

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+
 import SHOP_DATA from './shop.data';
 
 class ShopPage extends React.Component {
@@ -12,9 +14,14 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    return (
-      <div>SHOP PAGE</div>
-    )
+    const {collections} = this.state;
+    return (<div className='shop-page'>
+      {
+        collections.map(({id, ...otherCollectionProps}) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))
+      }  
+      </div>);
   }
 }
 
